108bd3b09abc4d51b3e6ebf19cfc884056cd718b
Kemal Eren <kemal@kemaleren.com>
2013-11-25 21:39:14 -0800

    fixed chunks

--- drug_names_picloud.py
+++ drug_names_picloud.py
@@ -8,7 +8,10 @@ import itertools
 import cloud
 from drug_names import map_drug_name
 
+
 CHUNK_SIZE = 10
+STEP_SIZE = 10000
+ALL_RESULTS = []
 
 
 def chunk_list(lst, chunk_size):
@@ -26,23 +29,26 @@ def function_dechunker(func):
 
 
 with open('unique_drugs.txt') as f:
-    names = f.read().split('\n')
+    NAMES = f.read().split('\n')
 
 
-jids = cloud.map(function_dechunker(map_drug_name),
-                 chunk_list(names, CHUNK_SIZE), _type="s1")
+for i in range(0, len(NAMES), STEP_SIZE):
+    name_chunk = NAMES[i: i + STEP_SIZE]
+    jids = cloud.map(function_dechunker(map_drug_name),
+                     chunk_list(name_chunk, CHUNK_SIZE), _type="s1")
 
-# a list of lists is returned
-chunked_results = cloud.result(jids)
+    # a list of lists is returned
+    chunked_results = cloud.result(jids)
 
-# merge lists to mimic non-chunked example
-results = list(itertools.chain.from_iterable(chunked_results))
+    # merge lists to mimic non-chunked example
+    results = list(itertools.chain.from_iterable(chunked_results))
+    ALL_RESULTS.extend(results)
 
 
 # save results
 with open('results.txt', 'w') as f:
     f.write("name	cuid	ingred_cuid	ingred\n")
-    for n, m in zip(names, results):
+    for n, m in zip(NAMES, ALL_RESULTS):
         cuid, ingred_cuid, ingred = m
         f.write("	".join([n, cuid, ingred_cuid, ingred]))
         f.write("\n")
