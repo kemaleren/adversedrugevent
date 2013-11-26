4b5acde7954c165cb6fd0854ea0b4d7ff80f8b79
Kemal Eren <kemal@kemaleren.com>
2013-11-25 20:44:37 -0800

    use soap

--- drug_names.py
+++ drug_names.py
@@ -6,41 +6,43 @@ import jellyfish
 import numpy as np
 
 # REST API of RxNav
-URL = "http://rxnav.nlm.nih.gov/REST/"
+from suds.client import Client
+url = "http://rxnav.nlm.nih.gov/RxNormDBService.xml"
+rxnav = Client(url)
 
 
-def _get_request(url):
-    """get and parse a REST request"""
-    r = requests.get(url)
-    if r.status_code != 200:
-        raise Exception('html error. status: {}'.format(r.status_code))
-    tree = ElementTree.fromstring(r.content)
-    return tree
+def approx_uis(drug):
+    """Returns the CUI and AUI of first approximate match in RxNav"""
+    result = rxnav.service.getApproximateMatch(drug, 1, 0)
+    if len(result.rxMatchInfo) == 0:
+        return None, None  # TODO: return unknown
+    r = result.rxMatchInfo[0]
+    return r.RXCUI, r.RXAUI
 
 
-def approx_cui(drug):
-    term = urllib2.quote(drug)
-    url = URL + "approximateTerm?term={}&maxEntries=1".format(term)
-    tree = _get_request(url)
-    try:
-        cand = tree.find('approximateGroup').find('candidate')
-        rxcui = cand.find('rxcui').text
-        return rxcui
-    except:
-        return None
-
-
-def map_drug_name_local(drug, db):
+def exact_uis(drug, db):
+    """Returns CUI and AUI of first exact match in RxNorm"""
     cur = db.cursor()
-    cur.execute("""select RXCUI from RXNCONSO where STR like "%{}%" limit 1;""".format(drug))
-    results = cur.fetchone()
+    n = cur.execute("""select RXCUI, RXAUI from RXNCONSO where STR like "%{}%" limit 1;""".format(drug))
+    if n == 0:
+        cur.close()
+        return None, None
+    result = cur.fetchone()
     cur.close()
-    return results is not None
+    rxcui = int(result[0])
+    rxaui = int(result[1])
+    return rxcui, rxaui
 
 
 def cui_to_canonical(cui, db):
     """Build a canonical name for this RXCUI"""
-    pass
+    cur = db.cursor()
+    n = cur.execute("""select STR from RXNCONSO where RXCUI like "{}" and TTY like "PN" limit 1;""".format(cui))
+    if n == 0:
+        cur.close()
+        return None
+    cur.close()
+    return cur.fetchone()
 
 
 def prefix_match(n, rxn_terms, min_length=5):
@@ -81,7 +83,7 @@ def match_vectors(word, targets, target_vectors, limit=2):
 
 
 def map_drug_name(drug, db):
-    """Returns the RxNorm precise ingredient for a drug."""
+    """Returns a precise name for each drug."""
     # TODO: what if search term matches multiple drugs?
     pass
 
