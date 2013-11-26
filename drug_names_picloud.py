74914b0796d2652247bfb01615d688e476803978
Kemal Eren <kemal@kemaleren.com>
2013-11-25 21:43:12 -0800

    fixed bug in writing results

--- drug_names_picloud.py
+++ drug_names_picloud.py
@@ -14,6 +14,12 @@ STEP_SIZE = 10000
 ALL_RESULTS = []
 
 
+with open('unique_drugs.txt') as f:
+    NAMES = f.read().split('\n')
+
+N_NAMES = len(NAMES)
+
+
 def chunk_list(lst, chunk_size):
     """Split a list into multiple lists of length chunk_size"""
     return [lst[i: i + chunk_size]
@@ -28,11 +34,8 @@ def function_dechunker(func):
     return wrapper_inner
 
 
-with open('unique_drugs.txt') as f:
-    NAMES = f.read().split('\n')
-
-
-for i in range(0, len(NAMES), STEP_SIZE):
+for i in range(0, N_NAMES, STEP_SIZE):
+    print "processing {} to {} of {}".format(i, i + STEP_SIZE, N_NAMES)
     name_chunk = NAMES[i: i + STEP_SIZE]
     jids = cloud.map(function_dechunker(map_drug_name),
                      chunk_list(name_chunk, CHUNK_SIZE), _type="s1")
@@ -50,5 +53,5 @@ with open('results.txt', 'w') as f:
     f.write("name	cuid	ingred_cuid	ingred\n")
     for n, m in zip(NAMES, ALL_RESULTS):
         cuid, ingred_cuid, ingred = m
-        f.write("	".join([n, cuid, ingred_cuid, ingred]))
+        f.write("	".join([str(n), str(cuid), str(ingred_cuid), str(ingred)]))
         f.write("\n")
